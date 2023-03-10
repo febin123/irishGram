3e3691e7197e8e009b1cf0107e50bc3aebcb7fb4
febin123 <febsep96@gmail.com>
2023-03-10 20:13:40 +0530

    adding verfication on the signup model

--- core/views.py
+++ core/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        if password1==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                 messages.info(request,'Username already exists')
+                 return redirect('signup')
+            else:
+                user=User.objects.create_user(username=username, email=email,password1=password1)
+                user.save()
+        else:
+            messages.info(request,'Password not matching')
+
+            return redirect('signup')
+
+    else:
+        return render(request,'signup.html')
