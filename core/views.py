96bae538ba711a757ee398369e4ea49e11c03db4
febin123 <febsep96@gmail.com>
2023-03-10 20:29:29 +0530

    log user in and redirect to settings page

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.http import HttpResponse
-
+from .models import Profile
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -23,8 +23,18 @@ def signup(request):
                  messages.info(request,'Username already exists')
                  return redirect('signup')
             else:
-                user=User.objects.create_user(username=username, email=email,password1=password1)
+                user=User.objects.create_user(username=username, email=email,password=password1)
                 user.save()
+
+                #log user in and redirect to settings page
+
+                #create a profile object for the new user
+
+                user_model=User.objects.get(username=username)
+
+                new_profile=Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request,'Password not matching')
 
